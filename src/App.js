efa8e631d5c4c4ca3fecb9660fc66cc59039cbc9
Sahar Mostafavi <168328421+sahar-14@users.noreply.github.com>
2024-10-31 23:06:59 -0700

    'add'

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a
+          className="App-link"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Hello world!
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
